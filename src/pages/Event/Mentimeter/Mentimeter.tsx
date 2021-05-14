2447fcab21fe18ad878b91c3009432a67ec9ffa7
Luca8991 <luca.berte98@hotmail.it>
2021-05-14 19:06:04 +0200

    feat: mentimeter embed

--- src/pages/Event/Mentimeter/Mentimeter.tsx
+++ src/pages/Event/Mentimeter/Mentimeter.tsx
@@ -15,19 +15,17 @@ const Mentimeter: React.FC<EventComponentProps> = (props: EventComponentProps) =
         <IonPage>
             <EventHeaderComponent event={props.event} />
             <IonContent>
-                <IonCard>
-                    <IonCardHeader>
-                        <IonCardTitle>
-                            Test
-                        </IonCardTitle>
-                        <IonCardSubtitle>
-                            Subtest
-                        </IonCardSubtitle>
-                    </IonCardHeader>
-                    <IonCardContent>
-                        MENTIMETER!
-                    </IonCardContent>
-                </IonCard>
+                <div style={{position: 'relative', height: '100%', overflow: 'hidden'}}>
+                    <iframe
+                        title={`menti-${props.event.id}`}
+                        sandbox='allow-scripts allow-same-origin'
+                        allowFullScreen={true}
+                        height='100%'
+                        width='100%'
+                        src={props.event.votingUrl}
+                        style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%',}}
+                    ></iframe>
+                </div>
             </IonContent>
         </IonPage>
     );
