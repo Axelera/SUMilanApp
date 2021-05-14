6bffb504b6810bb93f70ae714c2d46af47819acc
Luca8991 <luca.berte98@hotmail.it>
2021-05-14 11:49:04 +0200

    feat: live event first

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,6 +10,7 @@ import {
     IonPage,
     IonRow,
     IonSearchbar,
+    IonText,
     IonTitle,
     IonToolbar
 } from '@ionic/react';
@@ -20,15 +21,18 @@ import { search } from 'ionicons/icons';
 import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchEvents } from '../../store/actions/events/eventsActions';
-import { EventRelatorModel, EventStateModel } from '../../models/event.model';
+import { EventModel, EventRelatorModel, EventStateModel } from '../../models/event.model';
+import { EventTimeStatus, getEventTimeStatus } from '../../utils/eventTimeUtils';
+import { DateTime } from 'luxon';
 
 const Home: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
     const dispatch = useDispatch();
     const { items, loading, error } = useSelector<{ events: EventStateModel }, EventStateModel>(state => state.events);
-    
+
     const [isSearchbarVisible, setIsSearchbarVisible] = useState(false);
     const [searchQuery, setSearchQuery] = useState('');
-    const [filteredEvents, setFilteredEvents] = useState(items);
+    const [liveEvent, setLiveEvent] = useState<EventModel | undefined>();
+    const [filteredEvents, setFilteredEvents] = useState([...items]);
     const searchbarRef = useRef<HTMLIonSearchbarElement>(null);
 
     const toggleSearchbarHandler = () => {
@@ -52,17 +56,27 @@ const Home: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
     }
 
     useEffect(() => {
+        let fEvents = [...items];
         if (searchQuery) {
-            const fEvents = items.filter(ev => {
+            fEvents = items.filter(ev => {
                 return ev.title.toLowerCase().includes(searchQuery) ||
                     ev.type.toLowerCase().includes(searchQuery) ||
                     ev.description.toLowerCase().includes(searchQuery) ||
                     isQueryInRelators(searchQuery, ev.relators);
             });
-            setFilteredEvents([...fEvents]);
-        } else {
-            setFilteredEvents(items);
         }
+        const liveEvIndex = fEvents.findIndex(event => {
+            const dateTime = DateTime.fromISO(event.date);
+            return getEventTimeStatus(dateTime, event.duration) === EventTimeStatus.TODAY_LIVE;
+        })
+        let liveEv;
+        if (liveEvIndex >= 0) {
+            liveEv = fEvents.splice(liveEvIndex, 1)[0];
+        }
+        setFilteredEvents(fEvents.sort((a, b) => {
+            return DateTime.fromISO(b.date) > DateTime.fromISO(a.date) ? 0 : -1;
+        }));
+        setLiveEvent(liveEv);
     }, [searchQuery, items]);
 
     useEffect(() => {
@@ -100,7 +114,7 @@ const Home: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
                 </IonButtons>
             </IonToolbar>
         );
-    
+
     if (error) {
         return (
             <IonPage>
@@ -121,29 +135,48 @@ const Home: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
                     {toolbar}
                 </IonHeader>
                 <IonContent fullscreen>
-                <IonGrid style={{ padding: 0 }}>
-                    <IonRow>
-                        {[0, 1, 2].map((i, index) => {
-                            return (
-                                <IonCol
-                                    key={index}
-                                    sizeXs="12"
-                                    sizeSm="6"
-                                    sizeMd="6"
-                                    sizeLg="6"
-                                    sizeXl="4"
-                                    style={{ padding: 0 }}
-                                >
-                                    <EventCardComponent {...props} event={undefined} />
-                                </IonCol>
-                            );
-                        })}
-                    </IonRow>
-                </IonGrid>
+                    <IonGrid style={{ padding: 0 }}>
+                        <IonRow>
+                            {[0, 1, 2].map((i, index) => {
+                                return (
+                                    <IonCol
+                                        key={index}
+                                        sizeXs="12"
+                                        sizeSm="6"
+                                        sizeMd="6"
+                                        sizeLg="6"
+                                        sizeXl="4"
+                                        style={{ padding: 0 }}
+                                    >
+                                        <EventCardComponent {...props} event={undefined} />
+                                    </IonCol>
+                                );
+                            })}
+                        </IonRow>
+                    </IonGrid>
                 </IonContent>
             </IonPage>
         );
     }
+
+    const liveEventRow = liveEvent ? (
+        <IonRow>
+            <IonCol
+                key={Math.random()}
+                sizeXs="12"
+                sizeSm="6"
+                sizeMd="6"
+                sizeLg="6"
+                sizeXl="4"
+                style={{ padding: 0 }}
+            >
+                <IonText color="danger">
+                    <h4 style={{ marginLeft: 10 }}><b>In Onda Ora</b></h4>
+                </IonText>
+                <EventCardComponent {...props} event={liveEvent} />
+            </IonCol>
+        </IonRow>
+    ) : null;
 
     return (
         <IonPage>
@@ -152,6 +185,10 @@ const Home: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
             </IonHeader>
             <IonContent fullscreen>
                 <IonGrid style={{ padding: 0 }}>
+                    {liveEventRow}
+                    <IonText color="medium">
+                        <h4 style={{ marginLeft: 10 }}>Eventi in programma e passati</h4>
+                    </IonText>
                     <IonRow>
                         {filteredEvents?.length > 0 ? filteredEvents.map((event, index) => {
                             return (
