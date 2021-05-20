df4a67bbd20953bb38eb6a7b6032c0c6b8a80b68
Luca8991 <luca.berte98@hotmail.it>
2021-05-20 15:29:00 +0200

    feat: credits

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,18 @@
 import {
+  IonButton,
   IonContent,
+  IonFooter,
   IonIcon,
   IonItem,
   IonLabel,
   IonList,
+  IonListHeader,
   IonMenu,
   IonMenuToggle,
 } from '@ionic/react';
 
 import { useLocation } from 'react-router-dom';
-import { balloonOutline, balloonSharp, homeOutline, homeSharp, informationCircleOutline, informationCircleSharp, ticketOutline, ticketSharp } from 'ionicons/icons';
+import { balloonOutline, balloonSharp, globeOutline, homeOutline, homeSharp, informationCircleOutline, informationCircleSharp, logoGithub, ticketOutline, ticketSharp } from 'ionicons/icons';
 import './Menu.css';
 import LogoImage from './LogoImage/LogoImage';
 
@@ -61,6 +64,29 @@ const Menu: React.FC = () => {
           })}
         </IonList>
       </IonContent>
+      <IonFooter>
+        <IonList style={{ padding: 0 }}>
+          <IonListHeader>Credits:</IonListHeader>
+          <IonItem lines="full">
+            <IonLabel style={{ margin: 0 }}>
+              <h3>Luca Bertelli</h3>
+              <p>Sviluppo</p>
+            </IonLabel>
+            <IonButton slot="end" fill="clear" href="https://github.com/Luca8991" target="_blank">
+              <IonIcon slot="icon-only" icon={logoGithub} />
+            </IonButton>
+          </IonItem>
+          <IonItem lines="none">
+            <IonLabel style={{ margin: 0 }}>
+              <h3>Roberto Crosignani</h3>
+              <p>Marketing</p>
+            </IonLabel>
+            <IonButton slot="end" fill="clear" href="https://www.robertocrosignani.com/" target="_blank">
+              <IonIcon slot="icon-only" icon={globeOutline} />
+            </IonButton>
+          </IonItem>
+        </IonList>
+      </IonFooter>
     </IonMenu>
   );
 };
