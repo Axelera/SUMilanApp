53f70469e8859b0a19eeadb461f3f7b9c7011118
Luca8991 <luca.berte98@hotmail.it>
2021-05-28 17:12:24 +0200

    feat: chapter and young info

--- src/pages/Chapter/Chapter.tsx
+++ src/pages/Chapter/Chapter.tsx
@@ -6,9 +6,13 @@ import {
     IonPage,
     IonTitle,
     IonToolbar,
+    IonButton,
+    IonIcon,
+    IonText,
 } from "@ionic/react";
 import './Chapter.css';
-import LogoImage from "../../components/LogoImage/LogoImage";
+import { openOutline } from "ionicons/icons";
+import logo from '../../assets/images/logo-150x150.png';
 
 const Chapter: React.FC = () => {
     return (
@@ -22,8 +26,14 @@ const Chapter: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <LogoImage className="chapter-image" />
-                <p>Work in progress</p>
+                <div style={{ padding: 10, textAlign: 'center' }}>
+                    <img src={logo} alt="su-logo" />
+                    <p><b>SingularityU</b> <IonText color="secondary"><span><b>Milan</b></span></IonText> <IonText color="primary"><span><b>Chapter</b></span></IonText> è una comunità di attivisti che studiano le tecnologie esponenziali per affrontare le più grandi sfide del mondo e costruire un futuro migliore per tutti.</p>
+                    <IonButton href="https://www.singularityumilan.com/" target="_blank" color="primary" strong>
+                        Sito web
+                        <IonIcon slot="end" icon={openOutline} />
+                    </IonButton>
+                </div>
             </IonContent>
         </IonPage>
     );
--- src/pages/Young/Young.tsx
+++ src/pages/Young/Young.tsx
@@ -1,14 +1,18 @@
 import {
+    IonButton,
     IonButtons,
     IonContent,
     IonHeader,
+    IonIcon,
     IonMenuButton,
     IonPage,
+    IonText,
     IonTitle,
     IonToolbar,
 } from "@ionic/react";
 import './Young.css';
 import LogoImage from "../../components/LogoImage/LogoImage";
+import { logoInstagram } from "ionicons/icons";
 
 const Young: React.FC = () => {
     return (
@@ -22,8 +26,14 @@ const Young: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <LogoImage young={true} className="young-image"/>
-                <p>Work in progress</p>
+                <LogoImage young={true} className="young-image" />
+                <div style={{ padding: 10, textAlign: 'center' }}>
+                    <p>Gruppo di giovani attivisti del <b>SingularityU</b> <IonText color="secondary"><span><b>Milan</b></span></IonText> <IonText color="primary"><span><b>Chapter</b></span></IonText> che studiano le tecnologie esponenziali per affrontare le più grandi sfide del mondo e costruire un futuro migliore per tutti.</p>
+                    <IonButton href="https://www.instagram.com/singularityumilanyoung/" target="_blank" color="tertiary" strong>
+                        <IonIcon slot="start" icon={logoInstagram} />
+                        Pagina Instagram
+                    </IonButton>
+                </div>
             </IonContent>
         </IonPage>
     );
