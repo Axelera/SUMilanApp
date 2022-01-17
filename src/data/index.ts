8139c06af83140ae0f05cf92f63c41dc445b0f8d
Luca8991 <luca.berte98@hotmail.it>
2022-01-17 09:49:30 +0100

    👔 chapter social links #1

---
+++ src/data/index.ts
@@ -0,0 +1,8 @@
+export const SUM_SOCIAL_LINKS = {
+    'facebook': 'https://www.facebook.com/singularityumilan',
+    'twitter': 'https://twitter.com/singularityumi',
+    'instagram': 'https://www.instagram.com/singularityumilan/',
+    'youtube': 'https://www.youtube.com/c/singularityumilan',
+    'linkedin': 'https://www.linkedin.com/company/singularityu-milan-chapter/',
+    'website': 'https://www.singularityumilan.com/',
+};
--- src/pages/Chapter/Chapter.tsx
+++ src/pages/Chapter/Chapter.tsx
@@ -9,11 +9,12 @@ import {
     IonButton,
     IonIcon,
 } from "@ionic/react";
-import { openOutline } from "ionicons/icons";
+import { logoFacebook, logoInstagram, logoLinkedin, logoTwitter, logoYoutube, openOutline } from "ionicons/icons";
 import { Trans, useTranslation } from "react-i18next";
 
 import logo from '../../assets/images/logo-150x150.png';
 import ChapterName from "../../components/ChapterName/ChapterName";
+import { SUM_SOCIAL_LINKS } from "../../data";
 
 import './Chapter.css';
 
@@ -37,10 +38,29 @@ const Chapter: React.FC = () => {
                             <ChapterName /> is a community of activists studying exponential technologies to address the world's greatest challenges and build a better future for all.
                         </Trans>
                     </p>
-                    <IonButton href="https://www.singularityumilan.com/" target="_blank" color="primary" strong>
+                    <IonButton href={SUM_SOCIAL_LINKS.website} target="_blank" rel="noreferrer" color="primary" strong>
                         {t('CHAPTER.websiteButton')}
                         <IonIcon slot="end" icon={openOutline} />
                     </IonButton>
+                </div>
+                <div
+                    className="social-links-container"
+                >
+                    <a href={SUM_SOCIAL_LINKS.facebook} target="_blank" rel="noreferrer">
+                        <IonIcon slot="icon-only" icon={logoFacebook} size="large" />
+                    </a>
+                    <a href={SUM_SOCIAL_LINKS.twitter} target="_blank" rel="noreferrer">
+                        <IonIcon slot="icon-only" icon={logoTwitter} size="large" />
+                    </a>
+                    <a href={SUM_SOCIAL_LINKS.youtube} target="_blank" rel="noreferrer">
+                        <IonIcon slot="icon-only" icon={logoYoutube} size="large" />
+                    </a>
+                    <a href={SUM_SOCIAL_LINKS.linkedin} target="_blank" rel="noreferrer">
+                        <IonIcon slot="icon-only" icon={logoLinkedin} size="large" />
+                    </a>
+                    <a href={SUM_SOCIAL_LINKS.instagram} target="_blank" rel="noreferrer">
+                        <IonIcon slot="icon-only" icon={logoInstagram} size="large" />
+                    </a>
                 </div>
             </IonContent>
         </IonPage>
